1451f1aaba396d9e2e3aefe33beec90eaab1e292
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-07-11 18:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const codes = document.querySelectorAll(".code");
 
-// Focus first box initially
 codes[0].focus();
 
 codes.forEach((code, index) => {
 
-    // Move to next input after typing
     code.addEventListener("input", (e) => {
-
         // Allow only one digit
-        code.value = code.value.replace(/[^0-9]/g, "");
+        e.target.value = e.target.value.replace(/[^0-9]/g, "");
 
-        if (code.value !== "" && index < codes.length - 1) {
+        if (e.target.value !== "" && index < codes.length - 1) {
             codes[index + 1].focus();
         }
     });
 
-    // Handle Backspace
     code.addEventListener("keydown", (e) => {
 
         if (e.key === "Backspace") {
